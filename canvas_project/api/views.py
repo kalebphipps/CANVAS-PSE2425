c54f6ac6e052da9986ba42ad9a3af7fc35f2eadd
plhrt <pl.hrtr@gmail.com>
2025-01-14 17:01:57 +0100

    added the receiver list view

--- canvas_project/api/views.py
+++ canvas_project/api/views.py
@@ -95,3 +95,29 @@ class HeliostatDetail(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Heliostat.objects.filter(project__owner=self.request.user)
+
+
+class ReceiverList(generics.ListCreateAPIView):
+    """
+    Creates a view to list all receivers or to create a new one.
+    """
+
+    serializer_class = ReceiverSerializer
+
+    # Accepted authentication classes and the needed permissions to access the API
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    # Overwrite the default function to use the project defined by the project_id in the url for saving the heliostat
+    def perform_create(self, serializer):
+        project_id = self.kwargs["project_id"]
+        project = generics.get_object_or_404(
+            Project, id=project_id, owner=self.request.user
+        )
+        serializer.save(project=project)
+
+    def get_queryset(self):
+        project_id = self.kwargs["project_id"]
+        return Receiver.objects.filter(
+            project__id=project_id, project__owner=self.request.user
+        )
