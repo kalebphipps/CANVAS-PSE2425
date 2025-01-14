e14114d660334d4d7d9237b739c119c0698cfd30
usygz <usygz@student.kit.edu>
2025-01-14 18:27:36 +0100

    models are now registered in admin.py. Also, this push could contain new data for models.py and the html template, where models.py contains all models and the html contains a first outline of the GUI with not functionality however

--- canvas_project/project_management/admin.py
+++ canvas_project/project_management/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# Regestering all models
+admin.site.register(Project)
+admin.site.register(Heliostat)
+admin.site.register(Receiver)
+admin.site.register(Lightsource)
+admin.site.register(Settings)
