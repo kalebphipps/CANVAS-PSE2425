c7f19d10deb6679d4c88868a91a316f2ae32930f
plhrt <pl.hrtr@gmail.com>
2025-01-14 18:12:19 +0100

    added urls to urls.py

--- canvas_project/api/urls.py
+++ canvas_project/api/urls.py
@@ -1,5 +1,29 @@
 from django.urls import path
+from .views import (
+    ProjectList,
+    ProjectDetailList,
+    HeliostatList,
+    HeliostatDetail,
+    ReceiverList,
+    ReceiverDetail,
+    LightsourceList,
+    LightsourceDetail,
+    SettingsDetail,
+)
 
-from . import views
-
-urlpatterns = []
+urlpatterns = [
+    path("projects/", ProjectList.as_view()),
+    path("projects/<int:pk>/", ProjectDetailList.as_view()),
+    path("projects/<int:project_id>/heliostats/", HeliostatList.as_view()),
+    path(
+        "projects/<int:project_id>/heliostats/<int:pk>/",
+        HeliostatDetail.as_view(),
+    ),
+    path("projects/<int:project_id>/receivers/", ReceiverList.as_view()),
+    path("projects/<int:project_id>/receivers/<int:pk>/", ReceiverDetail.as_view()),
+    path("projects/<int:project_id>/lightsources/", LightsourceList.as_view()),
+    path(
+        "projects/<int:project_id>/lightsources/<int:pk>/", LightsourceDetail.as_view()
+    ),
+    path("projects/<int:project_id>/settings/", SettingsDetail.as_view()),
+]
