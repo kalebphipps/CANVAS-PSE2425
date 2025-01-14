98c2dbfeaa29913b28c952ee01bbbc18f761270a
plhrt <pl.hrtr@gmail.com>
2025-01-14 16:52:19 +0100

    added all serializers for the api

--- canvas_project/static/js/editor.mjs
+++ canvas_project/static/js/editor.mjs
@@ -2,64 +2,69 @@ import * as THREE from "three";
 
 import { Menu } from "menu";
 import { ViewHelper } from "compass";
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 let editorInstance = null;
 
 export class Editor {
-    constructor() {
-        // singleton
-        if (editorInstance) return editorInstance;
-        editorInstance = this;
+  constructor() {
+    // singleton
+    if (editorInstance) return editorInstance;
+    editorInstance = this;
 
-        this.menu = new Menu();
-        this.canvas = document.getElementById("canvas");
+    this.menu = new Menu();
+    this.canvas = document.getElementById("canvas");
 
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, this.canvas.clientWidth / this.canvas.clientHeight, 0.1, 1000);
-        this.camera.position.set(-7.5, 2.5, 0.75);
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      this.canvas.clientWidth / this.canvas.clientHeight,
+      0.1,
+      1000
+    );
+    this.camera.position.set(-7.5, 2.5, 0.75);
 
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        // since we render multiple times (scene and compass), we need to clear the renderer manually
-        this.renderer.autoClear = false;
-        this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
-        this.canvas.appendChild(this.renderer.domElement);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    // since we render multiple times (scene and compass), we need to clear the renderer manually
+    this.renderer.autoClear = false;
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+    this.canvas.appendChild(this.renderer.domElement);
 
-        this.geometry = new THREE.BoxGeometry(1, 1, 1);
-        this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        this.cube = new THREE.Mesh(this.geometry, this.material);
-        this.scene.add(this.cube);
+    this.geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    this.cube = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.cube);
 
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.gridHelper = new THREE.GridHelper(200, 50);
-        this.scene.add(this.gridHelper);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.gridHelper = new THREE.GridHelper(200, 50);
+    this.scene.add(this.gridHelper);
 
-        this.compass = new ViewHelper(this.camera, this.renderer.domElement, 256);
+    this.compass = new ViewHelper(this.camera, this.renderer.domElement, 256);
 
-        window.addEventListener('resize', () => this.onWindowResize());
+    window.addEventListener("resize", () => this.onWindowResize());
 
-        this.animate();
-    }
+    this.animate();
+  }
 
-    animate() {
-        requestAnimationFrame(() => this.animate());
-        this.render();
-    };
+  animate() {
+    requestAnimationFrame(() => this.animate());
+    this.render();
+  }
 
-    render() {
-        this.renderer.clear();
-        this.renderer.render(this.scene, this.camera);
-        this.compass.render(this.renderer);
-    };
+  render() {
+    this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
+    this.compass.render(this.renderer);
+  }
 
-    updateAspectRatio() {
-        this.camera.aspect = this.canvas.offsetWidth / this.canvas.offsetHeight;
-        this.camera.updateProjectionMatrix();
-    };
+  updateAspectRatio() {
+    this.camera.aspect = this.canvas.offsetWidth / this.canvas.offsetHeight;
+    this.camera.updateProjectionMatrix();
+  }
 
-    onWindowResize() {
-        this.updateAspectRatio();
-        this.renderer.setSize(this.canvas.offsetWidth, this.canvas.offsetHeight);
-        this.render();
-    };
+  onWindowResize() {
+    this.updateAspectRatio();
+    this.renderer.setSize(this.canvas.offsetWidth, this.canvas.offsetHeight);
+    this.render();
+  }
 }
