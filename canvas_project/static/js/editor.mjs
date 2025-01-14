b2b4f58514902af24d5160771eb05d577ff50946
plhrt <pl.hrtr@gmail.com>
2025-01-14 10:43:37 +0100

    added setUpScene() method

--- canvas_project/static/js/editor.mjs
+++ canvas_project/static/js/editor.mjs
@@ -96,7 +96,91 @@ export class Editor {
     this.render();
   }
 
-  #setUpScene() {}
+  #setUpScene() {
+    this.#canvas = document.getElementById("canvas");
+
+    this.#scene = new THREE.Scene();
+    this.#camera = new THREE.PerspectiveCamera(
+      75,
+      this.#canvas.clientWidth / this.#canvas.clientHeight,
+      0.1,
+      2000
+    );
+    this.#camera.position.set(-7.5, 2.5, 0.75);
+
+    this.#renderer = new THREE.WebGLRenderer({ antialias: true });
+    // since we render multiple times (scene and compass), we need to clear the renderer manually
+    this.#renderer.autoClear = false;
+    this.#renderer.shadowMap.enabled = true;
+    this.#renderer.setSize(this.#canvas.clientWidth, this.canvas.clientHeight);
+    this.#canvas.appendChild(this.#renderer.domElement);
+
+    //set up empty scene
+    this.#skyboxLoader = new THREE.CubeTextureLoader();
+    this.#skybox = this.#skyboxLoader.load([
+      "/static/img/skybox/px.png",
+      "/static/img/skybox/nx.png",
+      "/static/img/skybox/py.png",
+      "/static/img/skybox/ny.png",
+      "/static/img/skybox/pz.png",
+      "/static/img/skybox/nz.png",
+    ]);
+    this.#scene.background = this.#skybox;
+    this.#scene.fog = new THREE.Fog(0xdde0e0, 100, 2200);
+
+    this.#hemisphereLight = new THREE.HemisphereLight();
+    this.#scene.add(this.#hemisphereLight);
+
+    this.#terrain = new Terrain(2000);
+    this.#scene.add(this.#terrain);
+
+    this.#directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+    this.#directionalLight.position.set(200, 100, 200);
+    this.#directionalLight.castShadow = true;
+    this.#scene.add(this.#directionalLight);
+
+    this.#directionalLight.shadow.mapSize.set(16384, 16384);
+    this.#directionalLight.shadow.radius = 2;
+    this.#directionalLight.shadow.blurSamples = 10;
+    this.#directionalLight.shadow.camera.top = 200;
+    this.#directionalLight.shadow.camera.bottom = -200;
+    this.#directionalLight.shadow.camera.left = 400;
+    this.#directionalLight.shadow.camera.right = -400;
+    this.#directionalLight.shadow.camera.far = 1000;
+
+    // Helpers
+    this.#compass = new ViewHelper(
+      this.#camera,
+      this.#renderer.domElement,
+      200,
+      "circles"
+    );
+
+    this.#selectionBox = new THREE.BoxHelper();
+    this.#scene.add(this.#selectionBox);
+
+    // controls
+    this.#transformControls = new TransformControls(
+      this.#camera,
+      this.#renderer.domElement
+    );
+    this.#scene.add(this.#transformControls.getHelper());
+
+    this.#controls = new OrbitControls(this.#camera, this.#renderer.domElement);
+    this.#controls.screenSpacePanning = false;
+    this.#controls.maxDistance = 500;
+    this.#controls.minDistance = 10;
+    this.#controls.maxPolarAngle = Math.PI / 2 - 0.02;
+
+    // disable orbit controls while moving object
+    this.#transformControls.addEventListener("dragging-changed", (event) => {
+      this.#controls.enabled = !event.value;
+    });
+
+    this.#selectableGroup = new THREE.Group();
+    this.#selectableGroup.name = "selectableGroup";
+    this.#scene.add(this.#selectableGroup);
+  }
 
   async #loadProject() {}
 }
