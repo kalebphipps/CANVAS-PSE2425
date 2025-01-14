7bb767b4104a6b01a198bb0a9e140cf5db2cc8ac
plhrt <pl.hrtr@gmail.com>
2025-01-14 17:04:49 +0100

    added the lightsource list view

--- canvas_project/api/views.py
+++ canvas_project/api/views.py
@@ -136,3 +136,29 @@ class ReceiverDetail(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Receiver.objects.filter(project__owner=self.request.user)
+
+
+class LightsourceList(generics.ListCreateAPIView):
+    """
+    Creates a view to list all lightsources or to create a new one.
+    """
+
+    serializer_class = LightsourceSerializer
+
+    # Accepted authentication classes and the needed permissions to access the API
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    # Overwrite the default function to use the project_id for saving the heliostat
+    def perform_create(self, serializer):
+        project_id = self.kwargs["project_id"]
+        project = generics.get_object_or_404(
+            Project, id=project_id, owner=self.request.user
+        )
+        serializer.save(project=project)
+
+    def get_queryset(self):
+        project_id = self.kwargs["project_id"]
+        return Lightsource.objects.filter(
+            project__id=project_id, project__owner=self.request.user
+        )
