9f7cc420bd844a615190a7653f0c86309c434784
„Luana <luana@vitallowitz.de>
2025-01-17 14:02:45 +0100

    finished SingleObjectCommand class

--- canvas_project/static/js/singleObjectCommands.mjs
+++ canvas_project/static/js/singleObjectCommands.mjs
@@ -5,7 +5,7 @@ import { Command } from "command";
  * This class is designed for operations that target a single 'Object3D' instance.
  * It serves as a base class for commands that modify individual objects in the scene.
  */
-class SingleObjectCommand extends Command {
+export class SingleObjectCommand extends Command {
     /**
      * The object on which the command operates.
      * @type {Object3D}
@@ -19,21 +19,5 @@ class SingleObjectCommand extends Command {
     constructor(object) {
         super();
         this.#object = object;
-    }
-
-    /**
-     * Executes the operation on the specified 'Object3D' instance.
-     * The exact behavior depends on the subclass.
-     */
-    execute() {
-        super.execute();
-    }
-
-    /**
-     * Reverts the action performed by the 'execute()' method on the same 'Object3D' instance.
-     * The undo logic is specific to the subclass and restores the object to its previous state.
-     */
-    undo() {
-        super.undo();
     }
 }
