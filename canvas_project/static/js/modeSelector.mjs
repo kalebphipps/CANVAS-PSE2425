7f9f3a84485250d164bfc6b9a1d876bf5432f6eb
Lisann Engel <uumbm@student.kit.edu>
2025-01-20 22:07:19 +0100

    upps forgot to save but added event Listeners

---
+++ canvas_project/static/js/modeSelector.mjs
@@ -0,0 +1,67 @@
+import { TransformControls } from "THREE.transformControls";
+import { Editor } from "./editor.mjs";
+
+export class ModeSelector {
+    /** @type {THREE.TransformControls} */
+    #transformControls;
+    /** @type {Editor} */
+    #editor;
+    /** @type {string} */
+    #currentMode;
+    #translationButton;
+    #moveButton;
+    #rotateButton;
+
+    /**
+     *
+     * @param {THREE.TransformControls} transformControls
+     */
+    constructor(transformControls) {
+        this.#transformControls = transformControls;
+        this.#editor = Editor.getInstance();
+        this.#currentMode = "translate";
+
+        this.#translationButton = document.getElementById("home-tab2");
+        this.#moveButton = document.getElementById("contact-tab2");
+        this.#rotateButton = document.getElementById("profile-tab2");
+
+        this.addEventListeners();
+    }
+
+    /**
+     * add event listeners to the buttons
+     */
+    addEventListeners() {
+        this.#translationButton.addEventListener("click", () => {
+            this.setCurrentMode("translate");
+            this.#transformControls.setMode("translate");
+            this.#transformControls.detach();
+        });
+
+        this.#moveButton.addEventListener("click", () => {
+            this.setCurrentMode("move");
+            this.#transformControls.setMode("translate");
+        });
+
+        this.#rotateButton.addEventListener("click", () => {
+            this.setCurrentMode("rotate");
+            this.#transformControls.setMode("rotate");
+        });
+    }
+
+    /**
+     * set the current mode
+     * @param {string} mode
+     */
+    setCurrentMode(mode) {
+        this.#currentMode = mode;
+    }
+
+    /**
+     * get the current mode
+     * @returns the current mode
+     */
+    getCurrentMode() {
+        return this.#currentMode;
+    }
+}
