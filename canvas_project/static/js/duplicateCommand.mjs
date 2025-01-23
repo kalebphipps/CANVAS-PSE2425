b1c70dbfe6fd04290debf2d32f947a0475b1738d
plhrt <pl.hrtr@gmail.com>
2025-01-23 15:48:48 +0100

    added other commands and updated some methods and parameters to fit newest pull request

--- canvas_project/static/js/duplicateCommand.mjs
+++ canvas_project/static/js/duplicateCommand.mjs
@@ -1,9 +1,9 @@
 import { Editor } from "editor";
-import { Heliostat } from "objects";
+import { Heliostat, LightSource, Receiver } from "objects";
 import { SingleObjectCommand } from "singleObjectCommands";
 
 /**
- * Command to duplicate a heliost.
+ * Command to duplicate a heliostat.
  */
 export class DuplicateHeliostatCommand extends SingleObjectCommand {
     #editor = new Editor();
@@ -15,12 +15,7 @@ export class DuplicateHeliostatCommand extends SingleObjectCommand {
      * @param {Heliostat} heliostat the heliostat you want to duplicate
      */
     constructor(heliostat) {
-        super(heliostat);
-        this.#heliostat = heliostat;
-    }
-
-    async execute() {
-        console.log(this.#heliostat);
+        super();
         this.#heliostatCopy = new Heliostat(
             this.#heliostat.objectName == "" || !this.#heliostat.objectName
                 ? "Heliostat_Copy"
@@ -30,10 +25,91 @@ export class DuplicateHeliostatCommand extends SingleObjectCommand {
             this.#heliostat.numberOfFacets,
             this.#heliostat.kinematicType
         );
+        this.#heliostat = heliostat;
+    }
+
+    async execute() {
         await this.#editor.addHeliostat(this.#heliostatCopy);
     }
 
     undo() {
         this.#editor.deleteHeliostat(this.#heliostatCopy);
+    }
+}
+
+/**
+ * Command to duplicate a receiver.
+ */
+export class DuplicateReceiverCommand extends SingleObjectCommand {
+    #editor = new Editor();
+    #receiver;
+    #receiverCopy;
+
+    /**
+     * Create the command
+     * @param {Receiver} receiver the receiver you want to duplicate
+     */
+    constructor(receiver) {
+        super();
+        this.#receiverCopy = new Receiver(
+            this.#receiver.objectName == "" || !this.#receiver.objectName
+                ? "Receiver_Copy"
+                : this.#receiver.objectName + "_Copy",
+            this.#receiver.getPosition(),
+            this.#receiver.rotationY,
+            this.#receiver.normalVector,
+            this.#receiver.towerType,
+            this.#receiver.planeE,
+            this.#receiver.planeU,
+            this.#receiver.resolutionE,
+            this.#receiver.resolutionU,
+            this.#receiver.curvatureE,
+            this.#receiver.curvatureU
+        );
+        this.#receiver = receiver;
+    }
+
+    async execute() {
+        await this.#editor.addReceiver(this.#receiverCopy);
+    }
+
+    undo() {
+        this.#editor.deleteReceiver(this.#receiverCopy);
+    }
+}
+
+/**
+ * Command to duplicate a lightsource.
+ */
+export class DuplicateLightSourceCommand extends SingleObjectCommand {
+    #editor = new Editor();
+    #lightsource;
+    #lightsourceCopy;
+
+    /**
+     * Create the command
+     * @param {LightSource} lightsource the lightsource you want to duplicate
+     */
+    constructor(lightsource) {
+        super();
+        this.#lightsourceCopy = new LightSource(
+            this.#lightsource.objectName == "" || !this.#lightsource.objectName
+                ? "lightsource_Copy"
+                : this.#lightsource.objectName + "_Copy",
+            this.#lightsource.numberOfRays,
+            this.#lightsource.lightSourceType,
+            this.#lightsource.distributionType,
+            this.#lightsource.distributionMean,
+            this.#lightsource.distributionCovariance
+        );
+        this.#lightsource = lightsource;
+    }
+
+    async execute() {
+        await this.#editor.addLightsource(this.#lightsourceCopy);
+    }
+
+    undo() {
+        this.#editor.deleteLightsource(this.#lightsourceCopy);
     }
 }
