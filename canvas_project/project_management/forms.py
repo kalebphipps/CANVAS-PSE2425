c40b2c1bb6cf49ec6494cfeb8ae17e843a43a5f5
usygz <usygz@student.kit.edu>
2025-01-26 14:45:29 +0200

    Form style updated

--- canvas_project/project_management/forms.py
+++ canvas_project/project_management/forms.py
@@ -7,8 +7,18 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ["name", "description"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs.update({"class": "form-control"})
+        self.fields["description"].widget.attrs.update({"class": "form-control"})
+
 
 class UpdateProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ["name", "description"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs.update({"class": "form-control"})
+        self.fields["description"].widget.attrs.update({"class": "form-control"})
